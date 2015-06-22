0e298422291e97bd7e119c916ee44c0a6c430899
nxpeacock <nguyenxuancong@gmail.com>
2015-06-22 23:01:09 +0700

    change default size player

--- client/components/player.js
+++ client/components/player.js
@@ -4,8 +4,8 @@ var playComponent = FlowComponents.define('Player',function(props){
     this.description = props.description || '';
     this.url = props.url || '';
     this.thumbnail = props.thumbnail || '';*/
-    this.height = props.height || '476px';
-    this.width = props.width || '588px';
+    this.height = props.height || '360';
+    this.width = props.width || '480';
 
     var jwPlayerLib = 'https://jwpsrv.com/library/VoA6HNw9EeS0bxJtO5t17w.js';
     Meteor.Loader.loadJs(jwPlayerLib, function(){}, 1000);
@@ -14,13 +14,13 @@ var playComponent = FlowComponents.define('Player',function(props){
         var playerId = this.get('playerId');
 
         var JWPlayer = jwplayer(playerId).setup({
-            file: 'http://content.jwplatform.com/videos/HkauGhRi-640.mp4',
+            file: 'http://porn.im.2a721800.10505622.x.xvideos.com/videos/flv/a/5/8/xvideos.com_a58830d084666722ec636ed53967fe93.flv?e=1434996884&ri=1024&rs=85&h=6a83ad5bc46510071b70c0b9bd335464',
             height: this.height,
-            width: this.width
+            width : this.width
         });
 
         JWPlayer.onReady(function(){
-            JWPlayer.setControls(false);
+            //JWPlayer.setControls(false);
         })
     })
 
